320f46733f1b6b2689d39a5d7acbbf30e4ffb0a5
Sylvain Metayer <contact@sylvainmetayer.fr>
2018-02-03 22:58:07 +0100

    Update piwk tracking code

--- js/functions.js
+++ js/functions.js
@@ -1,27 +1,22 @@
 $(document).ready(function () {
     $(".button-collapse").sideNav();
 
-    var _paq = _paq || [];
-    _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-    _paq.push(["setCookieDomain", "*.sylvainmetayer.fr"]);
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    _paq.push(['enableHeartBeatTimer', 30]);
-    (function () {
-        var u = "//analytics.sylvainmetayer.fr/";
-        _paq.push(['setTrackerUrl', u + 'piwik.php']);
-        _paq.push(['setSiteId', '5']);
-        var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-        g.type = 'text/javascript';
-        g.async = true;
-        g.defer = true;
-        g.src = u + 'piwik.js';
-        s.parentNode.insertBefore(g, s);
+    <!-- Matomo -->
+    <script type="text/javascript">
+      var _paq = _paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(['trackPageView']);
+      _paq.push(['enableLinkTracking']);
+      (function() {
+        var u="//analytics.sylvainmetayer.fr/";
+        _paq.push(['setTrackerUrl', u+'piwik.php']);
+        _paq.push(['setSiteId', '1']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+      })();
+    </script>
+    <noscript><p><img src="//analytics.sylvainmetayer.fr/piwik.php?idsite=1&rec=1" style="border:0;" alt="" /></p></noscript>
+    <!-- End Matomo Code -->
 
-        gascrolldepth.init({
-            percentage: true,
-            PiwikPercentageA: true
-        });
-    })();
 });
 
